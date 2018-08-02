9e99e13ee04af60d01c874d14fdf9b385e3f4d38
cg33 <chg80333@163.com>
2018-08-02 14:44:27 +0800

    complete manage part

--- models/global.go
+++ models/global.go
@@ -2,9 +2,12 @@ package models
 
 // map下标是路由前缀，对应的值是GlobalTable类型，为表单与表格的数据抽象表示
 var GlobalTableList = map[string]GlobalTable{
-	"user":       GetUserTable(),
-	"manager":    GetManagerTable(),
-	"permission": GetPermissionTable(),
-	"roles":      GetRolesTable(),
-	"op":         GetOpTable(),
+	// 管理员管理部分
+	"manager":    GetManagerTable(),    // 管理员管理
+	"permission": GetPermissionTable(), // 权限管理
+	"roles":      GetRolesTable(),      // 角色管理
+	"op":         GetOpTable(),         // 操作日志管理
+
+	// 自定义管理部分
+	"user": GetUserTable(),
 }
