80d7a0d0e5e26b7cca1a72da1d9bbab99caeceb1
cg33 <chg80333@163.com>
2018-08-12 12:12:12 +0800

    session part: finish persitence

--- admin.sql
+++ admin.sql
@@ -367,6 +367,19 @@ VALUES
 UNLOCK TABLES;
 
 
+# Dump of table goadmin_session
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `goadmin_session`;
+
+CREATE TABLE `goadmin_session` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `sid` varchar(50) DEFAULT NULL,
+  `values` varchar(3000) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
--- auth/session.go
+++ auth/session.go
@@ -4,6 +4,8 @@ import (
 	"github.com/fasthttp-contrib/sessions"
 	"github.com/valyala/fasthttp"
 	"goAdmin/modules"
+	"goAdmin/connections/mysql"
+	"encoding/json"
 )
 
 type SessionHelper struct {
@@ -11,8 +13,14 @@ type SessionHelper struct {
 }
 
 func InitSessionHelper(ctx *fasthttp.RequestCtx) *SessionHelper {
+
+	var driver MysqlDriver
+	sessions.UseDatabase(&driver)
+
+	session := sessions.StartFasthttp(ctx)
+
 	return &SessionHelper{
-		sessions.StartFasthttp(ctx),
+		session,
 	}
 }
 
@@ -33,4 +41,29 @@ func (helper *SessionHelper) PutIntoSession(value string) string {
 	sessionKey := GenerateSessionId()
 	helper.Sess.Set(sessionKey, value)
 	return sessionKey
+}
+
+type MysqlDriver struct {}
+
+func (driver *MysqlDriver) Load(sid string) map[string]interface{} {
+	sesModel, _ := mysql.Query("select * from goadmin_session where sid = ?", sid)
+	if len(sesModel) < 1 {
+		return map[string]interface{}{}
+	} else {
+		var values map[string]interface{}
+		json.Unmarshal([]byte(sesModel[0]["values"].(string)), &values)
+		return values
+	}
+}
+
+func (driver *MysqlDriver) Update(sid string, values map[string]interface{}) {
+	if sid != "" && len(values) != 0 {
+		valuesByte, _ := json.Marshal(values)
+		sesModel, _ := mysql.Query("select * from goadmin_session where sid = ?", sid)
+		if len(sesModel) < 1 {
+			mysql.Exec("insert into goadmin_session (`values`, sid) values (?, ?)", string(valuesByte), sid)
+		} else {
+			mysql.Exec("update goadmin_session set `values` = ? where sid = ?", string(valuesByte), sid)
+		}
+	}
 }
