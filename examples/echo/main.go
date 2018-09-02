b813479f816973da3dfd66f734b6ba876dbad41e
cg33 <chg80333@163.com>
2018-09-02 08:44:35 +0800

    add support for echo

---
+++ examples/echo/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/chenhg5/go-admin"
+	"github.com/chenhg5/go-admin/modules/config"
+	echoFw "github.com/chenhg5/go-admin/framework/echo"
+	"github.com/chenhg5/go-admin/plugins/admin"
+	"github.com/chenhg5/go-admin/examples/datamodel"
+)
+
+func main() {
+	e := echo.New()
+
+	ad := goAdmin.Default()
+
+	// goAdmin 全局配置
+	cfg := config.Config{
+		DATABASE: config.Database{
+			IP:           "127.0.0.1",
+			PORT:         "3306",
+			USER:         "root",
+			PWD:          "root",
+			NAME:         "godmin",
+			MAX_IDLE_CON: 50,
+			MAX_OPEN_CON: 150,
+			DRIVER:       "mysql",
+		},
+		AUTH_DOMAIN:  "localhost",
+		LANGUAGE:     "cn",         // 语言
+		ADMIN_PREFIX: "admin", // 前缀
+	}
+
+	// 增删改查管理后台插件
+	adminPlugin := admin.NewAdmin(datamodel.TableFuncConfig)
+
+	ad.AddConfig(cfg).AddPlugins(adminPlugin).Use(new(echoFw.Echo), e)
+
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
---
+++ framework/echo/echo.go
@@ -0,0 +1,56 @@
+package echo
+
+import (
+	"errors"
+	"github.com/chenhg5/go-admin/plugins"
+	"strings"
+	"github.com/chenhg5/go-admin/context"
+	"bytes"
+	"github.com/labstack/echo"
+)
+
+type Echo struct {
+}
+
+func (e *Echo) Use(router interface{}, plugin []plugins.Plugin) error {
+	var (
+		engine *echo.Echo
+		ok     bool
+	)
+	if engine, ok = router.(*echo.Echo); !ok {
+		return errors.New("错误的参数")
+	}
+
+	for _, plug := range plugin {
+		var plugCopy plugins.Plugin
+		plugCopy = plug
+		for _, req := range plug.GetRequest() {
+			engine.Add(strings.ToUpper(req.Method), req.URL, func(i echo.Context) error {
+				return GetResponse(i, plugCopy)
+			})
+		}
+	}
+
+	return nil
+}
+
+func GetResponse(c echo.Context, plug plugins.Plugin) error {
+	ctx := context.NewContext(c.Request())
+
+	for _, key := range c.ParamNames() {
+		if c.Request().URL.RawQuery == "" {
+			c.Request().URL.RawQuery += strings.Replace(key, ":", "", -1) + "=" + c.Param(key)
+		} else {
+			c.Request().URL.RawQuery += "&" + strings.Replace(key, ":", "", -1) + "=" + c.Param(key)
+		}
+	}
+
+	plug.GetHandler(c.Request().URL.Path, strings.ToLower(c.Request().Method))(ctx)
+	for key, head := range ctx.Response.Header {
+		c.Response().Header().Set(key, head[0])
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(ctx.Response.Body)
+	c.String(ctx.Response.StatusCode, buf.String())
+	return nil
+}
