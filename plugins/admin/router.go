1c41bfd3dfffd680f23ec4c76c856044d2805b58
cg33 <chg80333@163.com>
2018-09-04 08:15:41 +0800

    auth filter

--- plugins/admin/router.go
+++ plugins/admin/router.go
@@ -10,34 +10,9 @@ import (
 func InitRouter(prefix string) *context.App {
 	app := context.NewApp()
 
-	// 仪表盘
-	if prefix != "" {
-		app.GET(prefix, auth.Middleware(controller.ShowDashboard, prefix))
-	} else {
-		app.GET("/", auth.Middleware(controller.ShowDashboard, prefix))
-	}
-
 	// 授权认证
 	app.GET(prefix + "/login", controller.ShowLogin)
 	app.POST(prefix + "/signin", controller.Auth)
-	app.GET(prefix + "/logout",  auth.Middleware(controller.Logout, prefix))
-
-	// 菜单管理
-	app.GET(prefix + "/menu",  auth.Middleware(controller.ShowMenu, prefix))
-	app.POST(prefix + "/menu/delete",  auth.Middleware(controller.DeleteMenu, prefix))
-	app.POST(prefix + "/menu/new",  auth.Middleware(controller.NewMenu, prefix))
-	app.GET(prefix + "/menu/new",  auth.Middleware(controller.ShowMenu, prefix))
-	app.POST(prefix + "/menu/edit",  auth.Middleware(controller.EditMenu, prefix))
-	app.GET(prefix + "/menu/edit/show",  auth.Middleware(controller.ShowEditMenu, prefix))
-	app.POST(prefix + "/menu/order",  auth.Middleware(controller.MenuOrder, prefix))
-
-	// 增删改查管理
-	app.GET(prefix + "/info/:prefix",  auth.Middleware(controller.ShowInfo, prefix))
-	app.GET(prefix + "/info/:prefix/edit",  auth.Middleware(controller.ShowForm, prefix))
-	app.GET(prefix + "/info/:prefix/new",  auth.Middleware(controller.ShowNewForm, prefix))
-	app.POST(prefix + "/edit/:prefix",  auth.Middleware(controller.EditForm, prefix))
-	app.POST(prefix + "/delete/:prefix",  auth.Middleware(controller.DeleteData, prefix))
-	app.POST(prefix + "/new/:prefix",  auth.Middleware(controller.NewForm, prefix))
 
 	// 自动化安装
 	app.GET(prefix + "/install", controller.ShowInstall)
@@ -49,6 +24,36 @@ func InitRouter(prefix string) *context.App {
 
 	for _, path := range template.GetComp("login").GetAssetList() {
 		app.GET(prefix + "/assets" + path, controller.Assert)
+	}
+
+	app.Group(prefix, auth.SetPrefix(prefix).Middleware)
+	{
+		// 仪表盘
+		if prefix != "" {
+			app.GET(prefix, controller.ShowDashboard)
+		} else {
+			app.GET("/", controller.ShowDashboard)
+		}
+
+		// 授权认证
+		app.GET(prefix + "/logout",  controller.Logout)
+
+		// 菜单管理
+		app.GET(prefix + "/menu",  controller.ShowMenu)
+		app.POST(prefix + "/menu/delete",  controller.DeleteMenu)
+		app.POST(prefix + "/menu/new",  controller.NewMenu)
+		app.GET(prefix + "/menu/new",  controller.ShowMenu)
+		app.POST(prefix + "/menu/edit",  controller.EditMenu)
+		app.GET(prefix + "/menu/edit/show",  controller.ShowEditMenu)
+		app.POST(prefix + "/menu/order",  controller.MenuOrder)
+
+		// 增删改查管理
+		app.GET(prefix + "/info/:prefix",  controller.ShowInfo)
+		app.GET(prefix + "/info/:prefix/edit",  controller.ShowForm)
+		app.GET(prefix + "/info/:prefix/new",  controller.ShowNewForm)
+		app.POST(prefix + "/edit/:prefix",  controller.EditForm)
+		app.POST(prefix + "/delete/:prefix",  controller.DeleteData)
+		app.POST(prefix + "/new/:prefix",  controller.NewForm)
 	}
 
 	return app
