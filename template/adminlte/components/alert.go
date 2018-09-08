22239c13a95b9800c5a20fcd0d92921b0078ef62
cg33 <chg80333@163.com>
2018-09-08 15:14:53 +0800

    add more componenets

---
+++ template/adminlte/components/alert.go
@@ -0,0 +1,32 @@
+package components
+
+import (
+	"github.com/chenhg5/go-admin/template/types"
+	"html/template"
+)
+
+type AlertAttribute struct {
+	Name    string
+	Theme   string
+	Title   template.HTML
+	Content template.HTML
+}
+
+func (compo *AlertAttribute) SetTheme(value string) types.AlertAttribute {
+	(*compo).Theme = value
+	return compo
+}
+
+func (compo *AlertAttribute) SetTitle(value template.HTML) types.AlertAttribute {
+	(*compo).Title = value
+	return compo
+}
+
+func (compo *AlertAttribute) SetContent(value template.HTML) types.AlertAttribute {
+	(*compo).Content = value
+	return compo
+}
+
+func (compo *AlertAttribute) GetContent() template.HTML {
+	return ComposeHtml(*compo, "alert")
+}
---
+++ template/adminlte/components/tabs.go
@@ -0,0 +1,20 @@
+package components
+
+import (
+	"github.com/chenhg5/go-admin/template/types"
+	"html/template"
+)
+
+type TabsAttribute struct {
+	Name   string
+	Data   []map[string]template.HTML
+}
+
+func (compo *TabsAttribute) SetData(value []map[string]template.HTML) types.TabsAttribute {
+	(*compo).Data = value
+	return compo
+}
+
+func (compo *TabsAttribute) GetContent() template.HTML {
+	return ComposeHtml(*compo, "tabs")
+}
