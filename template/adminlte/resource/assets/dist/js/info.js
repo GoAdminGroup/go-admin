298f673901cf6576c3e8b5c04b7cc375ab22cb7f
cg33 <chg80333@163.com>
2018-09-10 09:44:21 +0800

    add config

--- template/adminlte/resource/assets/dist/js/info.js
+++ template/adminlte/resource/assets/dist/js/info.js
@@ -69,8 +69,8 @@ $(function () {
 });
 
 function getQueryString(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    var r = window.location.search.substr(1).match(reg);
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
     if (r != null) {
         return decodeURI(r[2]);
     }
