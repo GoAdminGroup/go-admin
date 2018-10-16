e66da6f534643563de91e6fcf1d03bf951ab2d78
cg33 <chg80333@163.com>
2018-10-16 21:22:42 +0800

    add demo

---
+++ demo/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/chenhg5/go-admin/engine"
+	"github.com/chenhg5/go-admin/examples/datamodel"
+	"github.com/chenhg5/go-admin/modules/config"
+	"github.com/chenhg5/go-admin/plugins/admin"
+	"github.com/chenhg5/go-admin/plugins/example"
+	"github.com/chenhg5/go-admin/template/types"
+	_ "github.com/chenhg5/go-admin/adapter/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	eng := engine.Default()
+
+	cfg := config.Config{
+		DATABASE: []config.Database{
+			{
+				HOST:         "127.0.0.1",
+				PORT:         "3306",
+				USER:         "root",
+				PWD:          "root",
+				NAME:         "godmin",
+				MAX_IDLE_CON: 50,
+				MAX_OPEN_CON: 150,
+				DRIVER:       "mysql",
+			},
+		},
+		DOMAIN: "localhost",
+		PREFIX: "admin",
+		STORE: config.Store{
+			PATH:   "./uploads",
+			PREFIX: "uploads",
+		},
+		LANGUAGE: "cn",
+		INDEX:    "/",
+	}
+
+	adminPlugin := admin.NewAdmin(datamodel.Generators)
+
+	// you can custom a plugin like:
+
+	examplePlugin := example.NewExample()
+
+	if err := eng.AddConfig(cfg).AddPlugins(adminPlugin, examplePlugin).Use(r); err != nil {
+		panic(err)
+	}
+
+	r.Static("/uploads", "./uploads")
+
+	// you can custom your pages like:
+
+	r.GET("/"+cfg.PREFIX+"/custom", func(ctx *gin.Context) {
+		engine.Content(ctx, func() types.Panel {
+			return datamodel.GetContent()
+		})
+	})
+
+	r.Run(":9033")
+}
