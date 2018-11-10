b9287066e372ca3024ea409f41858a203d1f4ea1
cg33 <chg80333@163.com>
2018-11-10 17:11:11 +0800

    add more statement for adapter

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -5,6 +5,10 @@ import (
 	"github.com/chenhg5/go-admin/template/types"
 )
 
+// WebFrameWork is a interface which is used as an adapter of
+// framework and goAdmin. It must implement two methods. Use registers
+// the routes and the corresponding handlers. Content writes the
+// response to the corresponding context of framework.
 type WebFrameWork interface {
 	Use(interface{}, []plugins.Plugin) error
 	Content(interface{}, types.GetPanel)
