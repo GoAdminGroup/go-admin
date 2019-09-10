393f7f5ff37ce7753d37e619e26c63d10defca1e
cg33 <chg80333@163.com>
2019-09-10 08:18:38 +0800

    modify cli tool

--- admincli/cli.go
+++ admincli/cli.go
@@ -455,6 +455,14 @@ import "github.com/chenhg5/go-admin/plugins/admin/modules/table"
 
 // The key of Generators is the prefix of table info url.
 // The corresponding value is the Form and Table data.
+//
+// http://{{config.DOMAIN}}:{{PORT}}/{{config.PREFIX}}/info/{{key}}
+//
+// example:
+//
+// "posts"   => http://localhost:9033/admin/info/posts
+// "authors" => http://localhost:9033/admin/info/authors
+//
 var Generators = map[string]table.Generator{` + tableStr + `
 }
 `
