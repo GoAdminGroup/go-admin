492982b4ceaaf6831268b3eac4ca4160e04ab4b1
cg33 <chg80333@163.com>
2019-10-21 00:53:33 +0800

    improve cli and template

--- adm/compile_tmpl.go
+++ adm/compile_tmpl.go
@@ -10,7 +10,7 @@ import (
 func compileTmpl(rootPath, outputPath, packageName string) {
 	content := `package ` + packageName + `
 
-var templateList = map[string]string{`
+var TemplateList = map[string]string{`
 
 	content = getContentFromDir(content, fixPath(rootPath), fixPath(rootPath))
 
