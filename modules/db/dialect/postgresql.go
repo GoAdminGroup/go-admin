114e0f45b2862e44aa06e5d6f6e5034e90f208e4
cg33 <chg80333@163.com>
2019-10-31 11:32:40 +0800

    fix postgresql query error

--- modules/db/dialect/postgresql.go
+++ modules/db/dialect/postgresql.go
@@ -11,3 +11,9 @@ func (postgresql) GetName() string {
 func (postgresql) ShowTables() string {
 	return "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"
 }
+
+func (c postgresql) Select(comp *SqlComponent) string {
+	comp.Statement = "select " + comp.getFields(c.delimiter) + ` from "` + comp.TableName + `"` + comp.getJoins(c.delimiter) +
+		comp.getWheres(c.delimiter) + comp.getOrderBy() + comp.getLimit() + comp.getOffset()
+	return comp.Statement
+}
