f386465b2c22c3ec23a0a731fd23d5f458be4be6
laijinhang <30384322+laijinhang@users.noreply.github.com>
2020-01-08 16:27:16 +0800

    Create utils.go
    HTML content compression

---
+++ plugins/admin/utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"html/template"
+	"strings"
+)
+
+func CompressedCode(h *template.HTML) {
+	var s = string(*h)
+
+	st := strings.Split(s, "\n")
+	ss := []string{}
+	for i := 0;i < len(st);i++ {
+		st[i] = strings.TrimSpace(st[i])
+		if st[i] != "" {
+			ss = append(ss, st[i])
+		}
+	}
+	s = strings.Join(ss, "\n")
+	*h = template.HTML(s)
+}
