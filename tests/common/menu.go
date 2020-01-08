827763d645d31da9786fd876b7c374c74a3ddec8
cg33 <chg80333@163.com>
2020-01-08 10:53:29 +0800

    update tests

--- tests/common/menu.go
+++ tests/common/menu.go
@@ -55,7 +55,7 @@ func menuTest(e *httpexpect.Expect, sesID *http.Cookie) {
 
 	printlnWithColor("show form", "green")
 	formBody = e.GET(config.Get().Url("/menu/edit/show")).
-		WithQuery("id", "3").
+		WithQuery("__goadmin_edit_pk", "3").
 		WithCookie(sesID.Name, sesID.Value).
 		Expect().Status(200).Body()
 
