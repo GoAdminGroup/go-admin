44d96332e71e1f8808ffa084d58890b55af2e7d0
cg33 <chg80333@163.com>
2020-01-28 16:15:53 +0800

    Fixed: session life time set error

--- modules/auth/session.go
+++ modules/auth/session.go
@@ -71,7 +71,7 @@ func (ses *Session) Add(key string, value interface{}) {
 	cookie := http.Cookie{
 		Name:     ses.Cookie,
 		Value:    ses.Sid,
-		MaxAge:   7200,
+		MaxAge:   int(ses.Expires),
 		Expires:  time.Now().Add(ses.Expires),
 		HttpOnly: true,
 		Path:     "/",
