2aaca3d6ee8f07b795b5f3282f5591f4bdde3f1a
cg33 <chg80333@163.com>
2020-02-09 22:27:48 +0800

    update example

--- examples/datamodel/user.go
+++ examples/datamodel/user.go
@@ -64,16 +64,16 @@ func GetUserTable(ctx *context.Context) (userTable table.Table) {
 	info.AddActionButton("审批", action.Ajax("/admin/audit",
 		func(ctx *context.Context) (success bool, data, msg string) {
 			fmt.Println("PostForm", ctx.PostForm())
-			return true, "", "审批成功，奥利给"
+			return true, "", "success"
 		}))
-	info.AddButton("jump", icon.User, action.JumpInNewTab("/admin/info/authors", "作者"))
+	info.AddButton("jump", icon.User, action.JumpInNewTab("/admin/info/authors", "authors"))
 	info.AddButton("popup", icon.Terminal, action.PopUp("/admin/popup", "Popup Example",
 		func(ctx *context.Context) (success bool, data, msg string) {
 			return true, "<h2>hello world</h2>", ""
 		}))
 	info.AddButton("ajax", icon.Android, action.Ajax("/admin/ajax",
 		func(ctx *context.Context) (success bool, data, msg string) {
-			return true, "", "请求成功，奥利给"
+			return true, "", "success"
 		}))
 
 	info.SetTable("users").SetTitle("Users").SetDescription("Users")
