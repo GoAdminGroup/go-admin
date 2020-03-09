179a050bbda04591cf2d082ece63e07e5694be26
cg33 <chg80333@163.com>
2020-03-09 09:04:07 +0800

    fix(admin): fixed example error

--- examples/datamodel/user.go
+++ examples/datamodel/user.go
@@ -79,7 +79,7 @@ func GetUserTable(ctx *context.Context) (userTable table.Table) {
 	info.AddSelectBox("gender", types.FieldOptions{
 		{Value: "0", Text: "men"},
 		{Value: "1", Text: "women"},
-	}, action.FieldFilter("user", "gender"))
+	}, action.FieldFilter("gender"))
 
 	info.SetTable("users").SetTitle("Users").SetDescription("Users")
 
