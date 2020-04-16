b8dfe7554f0459bdba00c76db1f1b9b3973bedb1
cg33 <chg80333@163.com>
2020-04-16 10:58:46 +0800

    test(tests): add version check API test

--- template/template_test.go
+++ template/template_test.go
@@ -11,4 +11,8 @@ func TestVersionCompare(t *testing.T) {
 	assert.Equal(t, true, VersionCompare("v1.2.8", []string{"v1.2.5", "v1.2.8"}))
 	assert.Equal(t, false, VersionCompare("v1.2.7", []string{"v1.2.8"}))
 	assert.Equal(t, true, VersionCompare("v0.0.30", []string{"v0.0.30"}))
+	assert.Equal(t, true, VersionCompare("v0.0.30", []string{">=v0.0.30"}))
+	assert.Equal(t, true, VersionCompare("v0.0.30", []string{">=v0.0.29"}))
+	assert.Equal(t, false, VersionCompare("v0.0.30", []string{">=v0.1.1"}))
+	assert.Equal(t, true, VersionCompare("v0.0.30", []string{"<=v0.1.1"}))
 }
