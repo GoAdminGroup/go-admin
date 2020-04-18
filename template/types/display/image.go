3991dc655d17bd1621b1967c14efe3dcc86a5e1f
skanger <1035661353@qq.com>
2020-04-19 00:32:20 +0800

    refactor(admin): `FieldImage` update

--- template/types/display/image.go
+++ template/types/display/image.go
@@ -1,1 +1,26 @@
 package display
+
+import (
+	"github.com/GoAdminGroup/go-admin/template"
+	"github.com/GoAdminGroup/go-admin/template/types"
+)
+
+type Image struct{}
+
+func init() {
+	types.RegisterDisplayFnGenerator("image", new(Image))
+}
+
+func (image *Image) Get(args ...interface{}) types.FieldFilterFn {
+
+	param := args[2].([]interface{})
+	return func(value types.FieldModel) interface{} {
+		if len(param) > 0 {
+			return template.Default().Image().SetWidth(args[0].(string)).SetHeight(args[1].(string)).SetSrc(template.HTML(param[0].(string) + value.Value)).GetContent()
+
+		} else {
+			return template.Default().Image().SetWidth(args[0].(string)).SetHeight(args[1].(string)).SetSrc(template.HTML(value.Value)).GetContent()
+		}
+	}
+
+}
