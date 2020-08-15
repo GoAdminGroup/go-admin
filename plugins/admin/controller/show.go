c52308760abbb87a04d6964477fc05254ab8ca6a
cg33 <chg80333@163.com>
2020-08-15 16:16:27 +0800

    feat(admin): improve info table style

--- plugins/admin/controller/show.go
+++ plugins/admin/controller/show.go
@@ -176,13 +176,15 @@ func (h *Handler) showTable(ctx *context.Context, prefix string, params paramete
 		var content template2.HTML
 		content, actionJs = allActionBtns.Content()
 
-		actionBtns = html.Div(
+		actionBtns = html.Div(html.Div(
 			html.A(icon.Icon(icon.EllipsisV),
 				html.M{"color": "#676565"},
-				html.M{"class": "dropdown-toggle", "href": "#", "data-toggle": "dropdown"},
-			)+html.Ul(content,
+				html.M{"href": "#"},
+			), html.M{"cursor": "pointer", "width": "100%"}, html.M{"class": "dropdown-toggle", "data-toggle": "dropdown"})+
+			html.Ul(content,
 				html.M{"min-width": "20px !important", "left": "-32px", "overflow": "hidden"},
 				html.M{"class": "dropdown-menu", "role": "menu", "aria-labelledby": "dLabel"}),
+
 			html.M{"text-align": "center"}, html.M{"class": "dropdown"})
 	}
 
