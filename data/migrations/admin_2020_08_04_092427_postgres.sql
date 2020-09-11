45ef3595e0b04e59464e74db1ddf8717f3e4e953
cg33 <chg80333@163.com>
2020-09-11 10:47:12 +0800

    fix(data): fixed admin migration sql

--- data/migrations/admin_2020_08_04_092427_postgres.sql
+++ data/migrations/admin_2020_08_04_092427_postgres.sql
@@ -1,3 +1,3 @@
 ALTER TABLE goadmin_menu
-ADD COLUMN plugin_name character varying(100) NOT NULL,
-ADD COLUMN uuid character varying(100);
+ADD COLUMN plugin_name character varying(150) NOT NULL DEFAULT '',
+ADD COLUMN uuid character varying(150) NOT NULL DEFAULT '';
--- data/migrations/admin_2020_08_04_092427_sqlite.sql
+++ data/migrations/admin_2020_08_04_092427_sqlite.sql
@@ -1,3 +1,3 @@
 ALTER TABLE goadmin_menu
-ADD COLUMN `uuid` varchar(150) DEFAULT NULL,
+ADD COLUMN `uuid` varchar(150) NOT NULL DEFAULT '',
 ADD COLUMN `plugin_name` varchar(150) NOT NULL DEFAULT '';
