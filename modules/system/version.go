49ac2aa63edfa7f89bc62d63a133ba6e8f64823c
cg33 <chg80333@163.com>
2020-09-15 08:31:31 +0800

    feat(release): update version

--- modules/system/version.go
+++ modules/system/version.go
@@ -3,8 +3,8 @@ package system
 const version = "v1.2.17"
 
 var requireThemeVersion = map[string][]string{
-	"adminlte": {">=v0.0.38"},
-	"sword":    {">=v0.0.38"},
+	"adminlte": {">=v0.0.39"},
+	"sword":    {">=v0.0.39"},
 }
 
 // Version return the version of framework.
