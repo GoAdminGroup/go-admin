99d92f84d1c21c42a5a45adbc03592acd3148283
cg33 <chg80333@163.com>
2021-02-21 13:10:28 +0800

    refactor(comments): update comments

--- adm/project.go
+++ adm/project.go
@@ -323,6 +323,8 @@ package tables
 
 import "github.com/GoAdminGroup/go-admin/plugins/admin/modules/table"
 
+// Generators is a map of table models.
+//
 // The key of Generators is the prefix of table info url.
 // The corresponding value is the Form and Table data.
 //
